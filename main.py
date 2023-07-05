d7f2f1591f95e2eacd99cea75ffd982480991c95
y.yurkiv <evrus84@gmail.com>
2023-07-05 19:47:27 +0300

    Add app list header

--- main.py
+++ main.py
@@ -14,7 +14,11 @@ with col_2:
 Hello, my name is Yuriy.\n
 I am learning Python and I am very passionate about it.\n
 Here you can view the applications that I developed during my studies.\n
-Don't judge harshly, we all started once)))
     """
 
     st.info(about_me)
+
+app_list_header = """
+So here is my apps. Don't judge harshly, we all started once)))
+"""
+st.write(app_list_header)

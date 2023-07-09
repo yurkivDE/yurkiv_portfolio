dbfbc0d02a296d7ee596c21d645fff4faf470443
y.yurkiv <evrus84@gmail.com>
2023-07-09 21:32:41 +0300

    Add app list with data from csv file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import pandas
 import streamlit as st
 
-st.set_page_config(layout="wide")
+# st.set_page_config(layout="wide")
 
 col_1, col_2 = st.columns(2)
 
@@ -22,3 +23,21 @@ app_list_header = """
 So here is my apps. Don't judge harshly, we all started once)))
 """
 st.write(app_list_header)
+
+col_3, empty_col, col_4 = st.columns([1.5, 0.5, 1.5])
+
+df = pandas.read_csv("data.csv", sep=";")
+
+with col_3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image(f"images/{row['image']}")
+        st.write(f"[Source code]({row['url']})")
+
+with col_4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image(f"images/{row['image']}")
+        st.write(f"[Source code]({row['url']})")
